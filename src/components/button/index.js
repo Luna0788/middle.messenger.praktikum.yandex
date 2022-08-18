59bb00ec84c79db28b166d46bd4d332e4dfdc30c
Yuliya Safronova <safronova.yuv@gmail.com>
2022-08-18 10:18:58 +0400

    feat: add component button, add pages sign_in and sign_up, add styles

---
+++ src/components/button/index.js
@@ -0,0 +1,5 @@
+import template from './button.hbs';
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import './style.scss'
+
+Handlebars.registerPartial('button', template);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-const main = document.querySelector('main');
-main.textContent = 'Hello, world!';
+import sign_in_tpl from './pages/sign_in/sign_in.hbs'
+import sign_up_tpl from './pages/sign_up/sign_up.hbs'
+import './components/button'
+import './style.scss'
+
+window.addEventListener('DOMContentLoaded', () => {
+    const main = document.querySelector('.main');
+    const path = window.location.pathname;
+    switch (path) {
+        case '/src/pages/sign_in':
+            page = sign_in_tpl();
+            break;
+        case '/src/pages/sign_up':
+            page = sign_up_tpl();
+            break;
+        default:
+            page = sign_in_tpl();
+            break;
+    }
+    main.innerHTML = page;
+});
