ce5538ddf774832945f534b45d8ac13329a0fe3f
Yuliya Safronova <safronova.yuv@gmail.com>
2022-08-19 01:24:47 +0400

    feat: add input component, modify styles

---
+++ src/components/input/index.js
@@ -0,0 +1,5 @@
+import template from './input.hbs';
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import './style.scss'
+
+Handlebars.registerPartial('input', template);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import sign_in_tpl from './pages/sign_in/sign_in.hbs'
 import sign_up_tpl from './pages/sign_up/sign_up.hbs'
 import './components/button'
+import './components/input'
 import './style.scss'
 
 window.addEventListener('DOMContentLoaded', () => {
     const main = document.querySelector('.main');
     const path = window.location.pathname;
     switch (path) {
-        case '/src/pages/sign_in':
+        case '/pages/sign_in':
             page = sign_in_tpl();
             break;
-        case '/src/pages/sign_up':
+        case '/pages/sign_up':
             page = sign_up_tpl();
             break;
         default:
