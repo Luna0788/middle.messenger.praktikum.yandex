1929bd4466ba6664f0410c181d79b3733e51021b
Yuliya Safronova <safronova.yuv@gmail.com>
2022-08-21 00:54:36 +0400

    feat: add chatlist item component; modify chat page

---
+++ src/components/chatlist_item/index.js
@@ -0,0 +1,5 @@
+import template from './chatlist_item.hbs';
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+import './style.scss'
+
+Handlebars.registerPartial('chatlist_item', template);
--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,7 +1,39 @@
 import chat_tpl from './chat.hbs';
-
+import '../../components/Link';
+import '../../components/chatlist_item';
 import './style.scss';
+import avatar from '../../static/ava.png'
 
-const page = chat_tpl();
+
+const data = {
+    chat_list: [
+        {
+            id: "1",
+            avatar_src: "https://pps.whatsapp.net/v/t61.24694-24/227606097_1225481874604217_1628161383089212568_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AVwrqvtpqcR31Y4fiYdwsuHeSS-HeK5ruO8yDOd5j1gO_g&oe=63111631",
+            sender_name: "Андрей",
+            last_date_time: "10:49",
+            message_slice: "Изображение",
+            unread_count: "256"
+        },
+        {
+            id: "2",
+            avatar_src: avatar,
+            sender_name: "Андрей",
+            last_date_time: "10:49",
+            message_slice: "И Human Interface Guidelines и Material Design рекомендуют...",
+            unread_count: "20"
+        },
+        {
+            id: "3",
+            avatar_src: avatar,
+            sender_name: "Андрей",
+            last_date_time: "10:49",
+            message_slice: "И Human Interface Guidelines и Material Design рекомендуют...",
+            unread_count: "2"
+        }
+    ]
+};
+
+const page = chat_tpl(data);
 
 export default page;
